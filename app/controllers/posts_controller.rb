cc331badee74646601936caac4c3bee192d857c2
NitinBhasneia <ntiinbhasneria360@gmail.com>
2020-01-23 19:51:35 +0530

    Improved

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,33 @@
 class PostsController < ApplicationController
     def index
+        #This will be the main page
         @posts = Post.all
     end
 
     def show
+        #when we redirect to @post this is to be shown
         @post = Post.find(params[:id])
     end    
 
     def new
-
+        #For calling @post in new
+        @post =Post.new
     end
     
     def create
         #render plain: params[:post].inspect
+        #on clicking on submit in new.html.erb
         @post=Post.new(post_params)
 
-        @post.save()
+        if (@post.save())
         redirect_to @post
+        else 
+            render 'new'
+        end
     end
 
     def post_params
+        #For create
         params.require(:post).permit(:title, :body)
     end
 end
